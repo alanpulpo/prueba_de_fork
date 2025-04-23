ea3589e776ca5a0b6fd64a1d5b0b8ee09d5f3a2f
alanpulpo <alanlopezperez@hotmail.com>
2025-04-23 19:53:49 -0300

    Agrega nueva funcionalidad

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Ejemplo Flutter',
+      title: 'Prueba Git: Parte 2, punto 6',
       theme: ThemeData(
         // This is the theme of your application.
         //
